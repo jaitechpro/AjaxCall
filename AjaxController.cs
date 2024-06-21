ab05367555bea4aaed99b270d3f856061dee364a
jaitechpro <98378521+jaitechpro@users.noreply.github.com>
2024-06-21 16:30:52 +0530

    Add files via upload

---
+++ AjaxController.cs
@@ -0,0 +1,57 @@
+﻿using AjaxCrud.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AjaxCrud.Controllers
+{
+    public class AjaxController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public AjaxController(ApplicationDbContext context)
+        {
+            _context =   context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult EmployeeList()
+        {
+            var data = _context.Employees.ToList(); 
+            return new JsonResult(data);
+        }
+        [HttpPost]
+        public JsonResult AddEmployee(Employee employee)
+        {
+            var emp = new Employee()
+            {
+                FirstName = employee.FirstName,
+                LastName = employee.LastName,   
+            };
+            _context.Employees.Add(emp);    
+            _context.SaveChanges();
+            return new JsonResult("Data saved");   
+        }
+        public JsonResult Delete(int id)
+        {
+            var data= _context.Employees.Where(e=>e.Id==id).SingleOrDefault();
+            _context.Employees.Remove(data);    
+            _context.SaveChanges(); 
+            return new JsonResult("Data Deleted");
+        }
+        //[HttpPost]
+        public JsonResult Edit(int id)
+        {
+            var data=_context.Employees.Where(e=>e.Id == id).SingleOrDefault();
+            _context.SaveChanges();
+            return new JsonResult(data);
+        }
+
+        [HttpPost]
+        public JsonResult Update(Employee employee)
+        {
+            _context.Employees.Update(employee);
+            _context.SaveChanges();
+            return new JsonResult("Data updated");
+        }
+    }
+}
